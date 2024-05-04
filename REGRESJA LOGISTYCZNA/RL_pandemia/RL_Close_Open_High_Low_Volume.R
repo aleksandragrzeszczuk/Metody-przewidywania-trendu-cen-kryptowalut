78268399c72f5331f368bd6b2365b37a0ccdaf26
Aleksandra <149307338+aleksandragrzeszczuk@users.noreply.github.com>
2024-05-04 15:10:26 +0200

    Add files via upload

---
+++ REGRESJA LOGISTYCZNA/RL_pandemia/RL_Close_Open_High_Low_Volume.R
@@ -0,0 +1,107 @@
+library(quantmod)
+library(caret)
+library(TTR)
+library(glmnet)
+library(tidyverse)
+
+base_btc <- c("BTC-USD")
+getSymbols(Symbols = base_btc, src = "yahoo", from = "2020-01-01", to = "2022-02-23", auto.assign = TRUE)
+BTC <- as.data.frame(`BTC-USD`)
+
+BTC$Target <- ifelse(Delt(BTC$'BTC-USD.Close') > 0, 1, 0)
+
+add_lagged_features <- function(data, column_name, n_days) {
+  for (i in 1:n_days) {
+    lagged_column_name <- paste0(column_name, '_', i, 'd_back')
+    data <- data %>%
+      mutate("{lagged_column_name}" := lag(!!sym(column_name), n = i))
+  }
+  return(data)
+}
+
+BTC <- add_lagged_features(BTC, "BTC-USD.Close", 30)
+BTC <- add_lagged_features(BTC, "BTC-USD.Open", 30)
+BTC <- add_lagged_features(BTC, "BTC-USD.High", 30)
+BTC <- add_lagged_features(BTC, "BTC-USD.Low", 30)
+BTC <- add_lagged_features(BTC, "BTC-USD.Volume", 30)
+
+# view(BTC)
+
+BTC <- BTC[-(1:30),]
+
+# view(BTC)
+
+set.seed(255707)
+m <- floor(0.8 * nrow(BTC))
+train_data <- BTC[1:m, ]
+test_data <- BTC[(m+1):nrow(BTC), ]
+
+x_train <- model.matrix(~ . -1 - Target, data = train_data)
+y_train <- train_data$Target
+x_test <- model.matrix(~ . -1 - Target, data = test_data)
+y_test <- test_data$Target
+
+train_accuracy <- numeric(30)
+for (i in 1:30) {
+  close_vars <- paste(paste0("`BTC-USD.Close_", 1:i, "d_back`"), collapse = " + ")
+  open_vars <- paste(paste0("`BTC-USD.Open_", 1:i, "d_back`"), collapse = " + ")
+  high_vars <- paste(paste0("`BTC-USD.High_", 1:i, "d_back`"), collapse = " + ")
+  low_vars <- paste(paste0("`BTC-USD.Low_", 1:i, "d_back`"), collapse = " + ")
+  volume_vars <- paste(paste0("`BTC-USD.Volume_", 1:i, "d_back`"), collapse = " + ")
+  
+  formula <- as.formula(paste("Target ~", close_vars, "+", open_vars, "+", high_vars, "+", low_vars, "+", volume_vars))
+  
+  model <- glm(formula, data = train_data, family = binomial)
+  predict_train <- predict(model, newdata = train_data, type = "response")
+  class_train <- ifelse(predict_train > 0.5, 1, 0)
+  confusion_train <- confusionMatrix(as.factor(class_train), as.factor(train_data$Target))
+  predict_test <- predict(model, newdata = test_data, type = "response")
+  class_test <- ifelse(predict_test > 0.5, 1, 0)
+  
+  confusion_train <- confusionMatrix(as.factor(class_train), as.factor(train_data$Target))
+  confusion_test <- confusionMatrix(as.factor(class_test), as.factor(test_data$Target))
+  
+  train_accuracy[i] <- confusion_train$overall['Accuracy']
+  
+  cat("Model z opóźnieniem", i, "dni:\n")
+  cat("Accuracy - Train:", confusion_train$overall['Accuracy'], "\n")
+  cat("Accuracy - Test:", confusion_test$overall['Accuracy'], "\n\n")
+}
+
+# Wybór najlepszego modelu na podstawie najwyższej dokładności
+best_model_index <- which.max(train_accuracy)
+cat("Najlepszy model ma opóźnienie", best_model_index, "dni z dokładnością", train_accuracy[best_model_index], "na zbiorze treningowym.\n")
+
+# Wybór najlepszego modelu na podstawie najwyższej dokładności ZBIORU TESTOWEGO
+best_model_index_test <- which.max(test_accuracy)
+cat("Najlepszy model na zbiorze testowym ma opóźnienie", best_model_index_test, "dni z dokładnością", test_accuracy[best_model_index_test], ".\n")
+
+# Metryki na zbiorze treningowym
+confusion_train <- confusionMatrix(as.factor(class_train), as.factor(train_data$Target))
+
+TPR_train <- confusion_train$byClass['Sensitivity']
+FPR_train <- 1 - confusion_train$byClass['Specificity']
+TNR_train <- confusion_train$byClass['Specificity']
+PPV_train <- confusion_train$byClass['Pos Pred Value']
+NPV_train <- confusion_train$byClass['Neg Pred Value']
+
+cat("TPR (Train):", TPR_train, "\n")
+cat("FPR (Train):", FPR_train, "\n")
+cat("TNR (Train):", TNR_train, "\n")
+cat("PPV (Train):", PPV_train, "\n")
+cat("NPV (Train):", NPV_train, "\n")
+
+# Metryki na zbiorze testowym
+confusion_test <- confusionMatrix(as.factor(class_test), as.factor(test_data$Target))
+
+TPR_test <- confusion_test$byClass['Sensitivity']
+FPR_test <- 1 - confusion_test$byClass['Specificity']
+TNR_test <- confusion_test$byClass['Specificity']
+PPV_test <- confusion_test$byClass['Pos Pred Value']
+NPV_test <- confusion_test$byClass['Neg Pred Value']
+
+cat("TPR (Test):", TPR_test, "\n")
+cat("FPR (Test):", FPR_test, "\n")
+cat("TNR (Test):", TNR_test, "\n")
+cat("PPV (Test):", PPV_test, "\n")
+cat("NPV (Test):", NPV_test, "\n")
